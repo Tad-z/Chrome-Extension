a9a424bebe749e4ed2e70bc73867deb507079ada
Tad-z <david.akintade1000@gmail.com>
2023-09-13 10:49:05 +0100

    push

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoSanitize = require('express-mongo-sanitize');
+const cron = require('node-cron');
+const axios = require('axios')
 
 const app = express();
 require("dotenv").config();
 
 const main = require("./model/db");
-const personRouter = require("./routes/person.routes")
+const personRouter = require("./routes/person.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,12 +18,22 @@ app.use("/api", personRouter)
 
 const PORT = process.env.PORT || 5000;
 main()
-.then(() => {
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}.`);
-      });
-      return console.log("DB connected...");
-}).catch(console.error)
+      console.log(`Server is running on port ${PORT}.`);
+    });
+    return console.log("DB connected...");
+  })
+  .catch(console.error);
 
+// Schedule the cron job to make a request every 10 minutes to keep the API alive
+cron.schedule('*/10 * * * *', async () => {
+  try {
+    // Make a GET request to a specific endpoint (e.g., /api/keep-alive)
+    const response = await axios.get(`https://stage1-gyv4.onrender.com/api`)
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+  } console.error('Keep-alive request error:', error);
+});
 
-module.exports = app;
