b981e0884c661db2bf2a86c5fb30057ef20d10c3
Tad-z <david.akintade1000@gmail.com>
2023-10-01 13:41:06 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.listen(PORT, (): void => {
   console.log("SERVER IS UP ON PORT:", PORT);
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(express.urlencoded({ extended: true }));
 
